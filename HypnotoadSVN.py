0c8a525fc07b0906c8826f67b5ab83f8439d4512
Michael Hall <github@michaelhall.ca>
2015-02-22 19:03:19 -0500

    Parse logs for revision history

--- HypnotoadSVN.py
+++ HypnotoadSVN.py
@@ -5,6 +5,8 @@ import os.path
 import re
 from . import SvnProcess
 
+LOG_PARSE = r'-{10,}\nr(\d+) \| ([^|]+) \| ([^|]+) \| [^\n]+\n\n(.+)'
+
 class OpenReadOnlyCommand(sublime_plugin.WindowCommand):
     def run(self, file):
         path = file.replace("${packages}", sublime.packages_path())
@@ -54,8 +56,43 @@ class SvnUpdateRevisionCommand(SvnCommand):
         return
     def is_visible(self, paths=None):
         return super(SvnUpdateRevisionCommand, self).is_visible(paths=paths, versionned=True)
+    def on_select(self, index):
+        if index < 0:
+            return
+        if index >= len(self.revisions):
+            self.number = self.number * 2
+            self.get_revisions(self.number)
+        revision = self.revisions[index]
+        self.name = "SVN Update to revision (%s)" % revision
+        self.run_command('update -r %s' % revision, self.paths)
+    def parse_logs(self, raw_logs):
+        matches = re.findall(LOG_PARSE, raw_logs, re.M)
+        revisions = []
+        logs = []
+        show_more = len(matches) >= self.number
+        for revision, author, date, message in matches:
+            revisions.append(revision)
+            logs.append(revision + ": " + message)
+            if int(revision) is 1:
+                show_more = False
+        if (show_more):
+            logs.append('More revisions...')
+        self.revisions = revisions
+        self.logs = logs
+    def on_logs_available(self, process):
+        output = process.output()
+        print(output)
+        self.parse_logs(output)
+        sublime.active_window().show_quick_panel(self.logs, self.on_select)
+    def get_revisions(self, revisions):
+        SvnProcess.Process('Log', 'svn log -r HEAD:1 -l ' + str(revisions), self.paths, False, True, self.on_logs_available)
     def run(self, paths=None):
         if paths is None:
             return
         self.paths = paths
-        sublime.active_window().show_input_panel('Which revision', '', self.on_done_input, self.on_change_input, self.on_cancel_input)
+        show_history = self.get_setting("updateToRevisionHistory")
+        if (show_history):
+            self.number = self.get_setting('updateToRevisionHistorySize')
+            self.get_revisions(self.number)
+        else:
+            sublime.active_window().show_input_panel('Which revision', '', self.on_done_input, self.on_change_input, self.on_cancel_input)
