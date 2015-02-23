cd7fb9da23571d7c9ce3168714de80de2915ead2
Michael Hall <github@michaelhall.ca>
2015-02-23 18:42:53 -0500

    Get rid of newline that gets added to theme file.
    The first character should be part of the xml declaration, otherwise it
    may not parse properly in all cases.

--- util.py
+++ util.py
@@ -27,8 +27,7 @@ COLOR_SCHEME = '''
         </dict>
     </dict>
 '''
-THEME_XML_DEFINITION = '''
-<?xml version="1.0" encoding="UTF-8"?>
+THEME_XML_DEFINITION = '''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 '''
 SCHEMES = [
