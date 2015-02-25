8351f3a7b7f34215842576587b8f8e2dbbfa6db0
Michael Hall <github@michaelhall.ca>
2015-02-24 19:21:45 -0500

    Add different outputs
    Add ability to send output to output-panel or dialogs. Default is now
    “panel” instead of “tab”

--- lib/output.py
+++ lib/output.py
@@ -1,44 +1,87 @@
 import sublime
 import sublime_plugin
 import re
+from . import util
 
 VIEW_NAME = 'SVN Output'
+PANEL_ID = 'svn-output'
 SYNTAX = 'Packages/HypnotoadSVN/languages/SVN Output.tmLanguage'
 
 class SvnView(sublime_plugin.EventListener):
+    buffer = ""
     view = None
+    panel = None
     def get():
-        if SvnView.view is None or SvnView.view.window() is None:
-            view = sublime.active_window().new_file()
-            view.set_scratch(True)
-            view.set_name(VIEW_NAME)
-            view.set_syntax_file(SYNTAX)
-            view.set_read_only(True)
-            SvnView.view = view
-        return SvnView.view
+        output = util.get_setting("outputTo")
+        if output == "dialog":
+            return None
+        if output == "tab":
+            if SvnView.view is None or SvnView.view.window() is None:
+                view = sublime.active_window().new_file()
+                view.set_scratch(True)
+                view.set_name(VIEW_NAME)
+                view.set_syntax_file(SYNTAX)
+                view.set_read_only(True)
+                SvnView.view = view
+            return SvnView.view
+        if SvnView.panel is None:
+            panel = sublime.active_window().create_output_panel(PANEL_ID)
+            panel.set_syntax_file(SYNTAX)
+            SvnView.panel = panel
+            sublime.active_window().run_command(
+                'show_panel',
+                {
+                    'panel': 'output.'+PANEL_ID
+                }
+            )
+            return SvnView.panel
+    def message(message):
+        output = util.get_setting("outputTo")
+        if output == "dialog":
+            SvnView.buffer = SvnView.buffer + message + "\n"
+            return
+        view = SvnView.get()
+        if view is None:
+            return
+        view.run_command(
+            'svn_view_message',
+            {
+                "message": message
+            }
+        );
+    def end():
+        output = util.get_setting("outputTo")
+        if output == "dialog":
+            sublime.message_dialog(SvnView.buffer)
+        SvnView.buffer = ""
+    def focus():
+        view = SvnView.get()
+        if view is None:
+            return
+        view.window().focus_view(view)
+    def scroll_to_bottom():
+        view = SvnView.get()
+        if view is None:
+            return
+        point = view.text_to_layout(view.size())
+        view.set_viewport_position(point, True)
     def on_close(self, view):
         if view == SvnView.view:
             SvnView.view = None
+        if view == SvnPanel:
+            SvnView.panel = None
 
 
 def indent(text="", spaces=4):
     return " " * spaces + re.sub(r'\n', '\n' + " " * spaces, text)
 
 def add_message(message):
-    view = SvnView.get()
-    view.run_command(
-        'svn_view_message',
-        {
-            "message": message
-        }
-    );
+    SvnView.message(message)
 
 def add_command(name):
-    view = SvnView.get()
-    view.window().focus_view(view)
-    point = view.text_to_layout(view.size() - 1)
+    SvnView.focus()
+    SvnView.scroll_to_bottom()
     add_message("Command: " + name)
-    view.set_viewport_position(point, True)
 
 def add_files(paths=None):
     if paths is None:
@@ -57,4 +100,4 @@ def add_error(err):
         add_message("Error:\n" + indent(err))
 
 def end_command():
-    add_message("\n")
+    SvnView.end()
