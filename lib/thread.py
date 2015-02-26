d34aa06c0fcaf2c71ec1cbb2ff0405f60b94a3a5
Michael Hall <github@michaelhall.ca>
2015-02-26 16:34:59 -0500

    Fix an issue with async, non-logging processes
    If a process was threaded, but never read, then it would never "poll" as
    completed. Not really sure why.

--- lib/thread.py
+++ lib/thread.py
@@ -20,14 +20,18 @@ class Process:
         self.errorText = None
         self.loading = 0
         self.on_complete = on_complete
-        Process.active_processes.append(self)
         if log:
             output.add_command(self.name)
             output.add_message(output.indent(cmd + ' ' + self.get_path(paths)))
             output.add_files(self.paths)
             output.add_result_section()
-        self.process = Popen(cmd + ' ' + self.get_path(paths), stdout=PIPE, stderr=PIPE, shell=True, bufsize=20)
+        if not paths:
+            self.command = cmd
+        else:
+            self.command = cmd + ' ' + self.get_path(paths)
+        self.process = Popen(self.command, stdout=PIPE, stderr=PIPE, shell=True, universal_newlines=True)
         if async:
+            Process.active_processes.append(self)
             self.check_status()
         else:
             self.finish()
@@ -43,27 +47,31 @@ class Process:
 
     def finish(self):
         out, error = self.process.communicate()
-        self.outputText = out.decode('UTF-8')
-        self.errorText = error.decode('UTF-8')
+        self.outputText = out
+        self.errorText = error
         self.done = True
-        Process.active_processes.remove(self)
         if self.log:
             output.add_result(self.output())
             output.add_error(self.error(), self.process.returncode)
             output.end_command()
 
     def check_status(self):
-        if self.log and self.process.stdout.raw:
-            line = self.process.stdout.raw.readline().decode("UTF-8").strip('\r\n')
-            if line:
+        if self not in Process.active_processes:
+            return
+        if self.process.stdout:
+            line = self.process.stdout.readline().strip('\r\n')
+            self.lines.append(line)
+            if self.log and line:
                 output.add_result_message(line)
         if self.is_done():
+            last = self.process.stdout.read().strip('\r\n')
+            self.outputText = "\n".join(self.lines) + "\n" + last
             Process.active_processes.remove(self)
             sublime.status_message("Complete: " + self.name)
             if self.on_complete is not None:
                 self.on_complete(self)
             if self.log:
-                output.add_result_message(self.process.stdout.raw.read().decode("UTF-8").strip('\r\n'))
+                output.add_result_message(last)
                 output.add_error(self.error(), self.process.returncode)
                 output.end_command()
         else:
@@ -82,7 +90,7 @@ class Process:
             return self.process.stdout.read()
         out = self.process.stdout.read()
         if out is not None:
-            self.outputText = out.decode('UTF-8')
+            self.outputText = out
             return self.outputText
         return None
 
@@ -93,7 +101,7 @@ class Process:
             return self.process.stderr.read()
         error = self.process.stderr.read()
         if error is not None:
-            self.errorText = error.decode('UTF-8')
+            self.errorText = error
             return self.errorText
         return None
 
