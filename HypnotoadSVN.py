c07aeb185f5e3b96ea2272e89c84f8df8e33bc3d
Michael Hall <github@michaelhall.ca>
2015-02-28 16:15:43 -0500

    Removing the color scheme.

--- HypnotoadSVN.py
+++ HypnotoadSVN.py
@@ -1,10 +1,8 @@
 import sublime
 import sublime_plugin
 import os
-from .lib import color_scheme, menu
+from .lib import menu
 
 def plugin_loaded():
     """Handles the plugin loaded event"""
-    color_scheme.generate()
-    color_scheme.add_listener()
     menu.create_user_side_bar()
--- lib/settings.py
+++ lib/settings.py
@@ -7,42 +7,17 @@ LISTENER_PREFIX = 'HypnotoadSVN-'
 class Settings:
     """Interface for communicating with settings"""
     plugin = None
-    preferences = None
     def load():
-        """Loads the settings for the plugin and application"""
+        """Loads the settings for the plugin"""
         Settings.plugin = sublime.load_settings(SETTINGS_FILE)
-        Settings.preferences = sublime.load_settings(GLOBAL_PREFERENCES)
 
     def get(name, type=None, default=None):
         """Gets a value from the plugin settings"""
-        if not Settings.plugin or not Settings.preferences:
+        if not Settings.plugin:
             Settings.load()
         if type is not None:
             return Settings.plugin.get(type, {}).get(name, default)
         return Settings.plugin.get(name, default)
-
-    def listen_changes(name, observer):
-        """Listens to changes in the settings"""
-        if not Settings.plugin or not Settings.preferences:
-            Settings.load()
-        Settings.unlisten_changes(name)
-        Settings.preferences.add_on_change(LISTENER_PREFIX + name, observer)
-        Settings.plugin.add_on_change(LISTENER_PREFIX + name, observer)
-
-    def unlisten_changes(name):
-        """Stops listening to changes in the settings"""
-        if not Settings.plugin or not Settings.preferences:
-            Settings.load()
-        Settings.preferences.clear_on_change(LISTENER_PREFIX + name)
-        Settings.plugin.clear_on_change(LISTENER_PREFIX + name)
-
-def listen_to_changes(name, observer):
-    """Add an observer for settings changes"""
-    Settings.listen_changes(name, observer)
-
-def unlisten_to_changes(name):
-    """Remove an observer for settings changes"""
-    Settings.unlisten_changes(name)
 
 def get(name, svn_type=None, default=None):
     """Gets a value from settings"""
