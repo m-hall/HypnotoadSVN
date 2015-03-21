3a552b422cc99e6e285d1a6f7940dadbc246f34b
Michael Hall <github@michaelhall.ca>
2015-03-20 20:31:08 -0400

    Make the branch command work properly.
    Get source from file info.
    Get destination from user input.

--- branch_commands.py
+++ branch_commands.py
@@ -5,6 +5,8 @@ import re
 from . import svn_commands
 from .lib import util, thread, settings, output, panels
 
+INFO_PARSE_URL = r"URL: ([^\n]*)"
+
 class HypnoSvnMergeCommand(svn_commands.HypnoSvnCommand):
     """Merges changes from the repo to the working copy"""
     def run(self, paths=None, group=-1, index=-1):
@@ -43,6 +45,21 @@ class HypnoSvnSwitchCommand(svn_commands.HypnoSvnCommand):
 
 class HypnoSvnBranchCommand(svn_commands.HypnoSvnCommand):
     """Creates a new branch"""
+
+    def get_url(self, file):
+        """Runs a native command to verify if a file is versionned"""
+        p = self.run_command('info', [file], False, False)
+        m = re.search(INFO_PARSE_URL, p.output(), re.M)
+        return m.group(1)
+
+    def on_done_input(self, value):
+        """Handles completion of an input panel"""
+        if value is None or not util.is_url(value):
+            sublime.status_message("Invalid URL")
+            return
+        self.name = "Branch"
+        items = [self.url, value]
+        self.run_command('copy', items)
     def run(self, paths=None, group=-1, index=-1):
         """Runs the command"""
         util.debug('Branch')
@@ -51,7 +68,9 @@ class HypnoSvnBranchCommand(svn_commands.HypnoSvnCommand):
         if util.use_tortoise():
             self.run_tortoise("branch", files)
             return
-        self.run_command('copy', files)
+
+        self.url = self.get_url(files[0])
+        sublime.active_window().show_input_panel('Branch to...', self.url, self.on_done_input, self.nothing, self.nothing)
     def is_visible(self, paths=None, group=-1, index=-1):
         """Checks if the command should be visible"""
         files = util.get_files(paths, group, index)
