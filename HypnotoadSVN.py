c8afb4dc5d1dfdc3c3d9f3711274b3067e0612b0
Guillaume Bibaut <yom@iaelu.net>
2015-03-31 10:11:36 +0200

    plugin menu removal it is nice to clean up once gone

--- HypnotoadSVN.py
+++ HypnotoadSVN.py
@@ -4,3 +4,8 @@ from .lib import menu
 def plugin_loaded():
     """Handles the plugin loaded event"""
     menu.create_user_side_bar()
+
+
+def plugin_unloaded():
+    """Handles the plugin unloaded event"""
+    menu.remove_user_side_bar()
