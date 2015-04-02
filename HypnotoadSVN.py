525c8fcae7a53616078f319452a80ad06623c9ba
Guillaume Bibaut <yom@iaelu.net>
2015-04-02 12:41:52 +0200

    using Package Control Events

--- HypnotoadSVN.py
+++ HypnotoadSVN.py
@@ -1,4 +1,7 @@
 from .lib import menu
+from package_control import events
+
+HYPNOTOADSVN_PKGNAME = "HypnotoadSVN"
 
 
 def plugin_loaded():
@@ -8,4 +11,5 @@ def plugin_loaded():
 
 def plugin_unloaded():
     """Handles the plugin unloaded event"""
-    menu.remove_user_side_bar()
+    if events.remove(HYPNOTOADSVN_PKGNAME) is not False:
+        menu.remove_user_side_bar()
