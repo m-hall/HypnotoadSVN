fb428bad0d7c3171c4d29490f5b89c67f1b543aa
Michael Hall <github@michaelhall.ca>
2015-04-04 23:31:11 -0400

    Add command to reset the side bar to default.
    This will make it so the users will be able to upgrade more easily when
    a new command is added.

--- other_commands.py
+++ other_commands.py
@@ -1,6 +1,6 @@
 import sublime
 import sublime_plugin
-from .lib import thread
+from .lib import thread, menu
 
 
 class HypnoKillProcessesCommand(sublime_plugin.WindowCommand):
@@ -9,3 +9,12 @@ class HypnoKillProcessesCommand(sublime_plugin.WindowCommand):
     def run(self):
         """Runs the command"""
         thread.terminate_all()
+
+
+class HypnoResetSideBarCommand(sublime_plugin.WindowCommand):
+    """A command that resets the side bar to the default"""
+
+    def run(self):
+        """Runs the command"""
+        menu.remove_user_side_bar()
+        menu.create_user_side_bar()
