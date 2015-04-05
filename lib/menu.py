5c93bb2471cad280b9f837f3c8a59931ac178dcd
Michael Hall <github@michaelhall.ca>
2015-04-04 23:31:35 -0400

    Cleanup the menu file a bit.
    Also if the folder is not empty, don’t remove it. The user may have
    added other menus, so let them keep their files.

--- lib/menu.py
+++ lib/menu.py
@@ -1,25 +1,27 @@
 import sublime
 import os
 
-DEFAULT_SIDE_BAR_FILE = "Packages/HypnotoadSVN/menus/Default Side Bar.sublime-menu"
-USER_SIDE_BAR_FILE = "/User/HypnotoadSVN/Side Bar.sublime-menu"
+DEFAULT_SIDE_BAR_RESOURCE = "Packages/HypnotoadSVN/menus/Default Side Bar.sublime-menu"
+USER_SIDE_BAR_FILE = "Side Bar.sublime-menu"
 
 
 def create_user_side_bar():
     """Create the sidebar config in the user directory"""
-    if os.path.exists(sublime.packages_path() + USER_SIDE_BAR_FILE):
+    user_folder = os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN')
+    user_file = os.path.join(user_folder, USER_SIDE_BAR_FILE)
+    if os.path.exists(user_file):
         return
-    if not os.path.exists(os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN')):
-        os.makedirs(os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN'))
-
-    side_bar_contents = sublime.load_resource(DEFAULT_SIDE_BAR_FILE)
-
-    with open(sublime.packages_path() + USER_SIDE_BAR_FILE, 'w', encoding='utf8') as f:
+    if not os.path.exists(user_folder):
+        os.makedirs(user_folder)
+    side_bar_contents = sublime.load_resource(DEFAULT_SIDE_BAR_RESOURCE)
+    with open(user_file, 'w', encoding='utf8') as f:
         f.write(side_bar_contents)
 
 
 def remove_user_side_bar():
     """Remove the sidebar config in the user directory"""
-    if os.path.exists(os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN')):
-        os.remove(sublime.packages_path() + USER_SIDE_BAR_FILE)
-        os.rmdir(os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN'))
+    user_folder = os.path.join(sublime.packages_path(), 'User', 'HypnotoadSVN')
+    if os.path.exists(user_folder):
+        os.remove(os.path.join(user_folder, USER_SIDE_BAR_FILE))
+    if not os.listdir(user_folder): # folder is empty
+        os.rmdir(user_folder)
